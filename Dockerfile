930a897215aa7c687365683d8752904154af42b2
sudoaj <salamajayi@yahoo.com>
2023-01-04 23:11:15 -0500

    updated to latest angular and added dockerfile

--- Dockerfile
+++ Dockerfile
@@ -9,6 +9,7 @@ WORKDIR /usr/local/app
 # Add the source code to app
 COPY ./ /usr/local/app/
 
+RUN node --version
 # Install all the dependencies
 RUN npm install
 
@@ -22,7 +23,7 @@ RUN npm run build
 FROM nginx:latest
 
 # Copy the build output to replace the default nginx contents.
-COPY --from=build /usr/local/app/dist/sample-angular-app /usr/share/nginx/html
+COPY --from=build /usr/local/app/dist/aj-playground /usr/share/nginx/html
 
 # Expose port 80
 EXPOSE 80
--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,13 +1,15 @@
 import { NgModule } from '@angular/core';
-import { Routes, RouterModule } from '@angular/router';
+import { Routes, RouterModule,ExtraOptions } from '@angular/router';
 import {HomeComponent} from './home/home.component'
 const routes: Routes = [
   {path: '', component: HomeComponent},
   {path: 'home', component: HomeComponent}
 ];
-
+const options: ExtraOptions = {
+  onSameUrlNavigation: 'reload',
+}
 @NgModule({
-  imports: [RouterModule.forRoot(routes, { relativeLinkResolution: 'legacy' })],
+  imports: [RouterModule.forRoot(routes,options)],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
